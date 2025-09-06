7133f49d6c0b42cf50c7dcd828bf39aeeb6ac1c6
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2025-09-06 18:22:27 -0400

    Refactor main function to prompt user for SDK Manager and AVD setup

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{env, io::Cursor};
 use bytes::{BufMut, Bytes, BytesMut};
 use color_eyre::eyre::{Context, bail};
 
-use crate::constants::{ANDROID_SDK_TOOLS, android_sdk_path};
+use crate::constants::{ANDROID_SDK_TOOLS, android_sdk_path, emulator_path};
 
 mod constants;
 mod downloader;
@@ -13,33 +13,59 @@ fn main() -> color_eyre::Result<()> {
     let sdk_manager = constants::sdkmanager_path();
 
     if !sdk_manager.exists() {
-        setup_sdk_manager().context("Failed to set up SDK Manager")?;
+        let accepted = dialoguer::Confirm::new()
+            .with_prompt("Android SDK Manager not found. Do you want to download and set it up?")
+            .interact()?;
+        if accepted {
+            setup_sdk_manager().context("Failed to set up SDK Manager")?;
+        }
     }
 
-    let status = std::process::Command::new(sdk_manager)
-        .arg("emulator")
-        .arg("platform-tools;system-images;android-33;android-desktop;x86_64")
-        .status()
-        .context("Failed to run sdkmanager")?;
+    let android_emu_image = dialoguer::Confirm::new()
+        .with_prompt("Do you want to install the Android Emulator and system image?")
+        .interact()?;
+    if android_emu_image {
+        let status = std::process::Command::new(&sdk_manager)
+            .arg("emulator")
+            .arg("platform-tools;system-images;android-33;android-desktop;x86_64")
+            .status()
+            .context("Failed to run sdkmanager")?;
 
-    if !status.success() {
-        bail!("sdkmanager exited with status: {}", status);
+        if !status.success() {
+            bail!("sdkmanager exited with status: {}", status);
+        }
     }
 
     // TODO: Check if emulator image is already installed
-    let status = std::process::Command::new(constants::avdmanager_path())
-        .arg("create")
-        .arg("avd")
-        .arg("-n")
-        .arg("android13desktop")
-        .arg("-k")
-        .arg("system-images;android-33;android-desktop;x86_64")
-        .status()
-        .context("Failed to run avdmanager")?;
 
-    if !status.success() {
-        bail!("avdmanager exited with status: {}", status);
+    let create_avd = dialoguer::Confirm::new()
+        .with_prompt("Do you want to create an AVD (Android Virtual Device)?")
+        .interact()?;
+    if create_avd {
+        let status = std::process::Command::new(constants::avdmanager_path())
+            .arg("create")
+            .arg("avd")
+            .arg("-n")
+            .arg("android13desktop")
+            .arg("-k")
+            .arg("system-images;android-33;android-desktop;x86_64")
+            .status()
+            .context("Failed to run avdmanager")?;
+
+        if !status.success() {
+            bail!("avdmanager exited with status: {}", status);
+        }
     }
+
+    println!("Setup complete! You can now run the emulator using the 'emulator' command.");
+    println!(
+        "Add {} to your PATH.",
+        sdk_manager.parent().unwrap().display()
+    );
+    println!(
+        "Add {}/emulator to your PATH.",
+        emulator_path().parent().unwrap().display()
+    );
 
     Ok(())
 }
